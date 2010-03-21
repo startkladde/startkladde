66ee40bb6ab44cf699de2b1f65b89309fc71d1c8
Martin Herrmann <martin@famherrmann.de>
2010-03-21 11:24:09 +0000

    Removed sample configuration file Updated documentation to reflect new configuration
    git-svn-id: svn://svn.code.sf.net/p/startkladde/code/branches/database_cleanup@499 ffb59c96-f4eb-4fd4-83d4-e68da7e46744

--- src/db/migrations/Migration_20100216135637_add_launch_methods.h
+++ src/db/migrations/Migration_20100216135637_add_launch_methods.h
@@ -11,6 +11,11 @@ class LaunchMethod;
 /**
  * Adds the launch methods table (and imports the launch methods from the
  * configuration file)
+ *
+ * Also imports old (configured) launch methods from ~/.startkladde.conf or
+ * ./startkladde.conf:
+ *   startart <id>, <type>, <towplane_registration>, <name>, <short_name>, <keyboard_shortcut>, <log_string>, <person_required>
+ *
  */
 class Migration_20100216135637_add_launch_methods: public Migration
 {
