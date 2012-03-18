fb898186f035bc8b5e7470223b3671dec114a2c1
Martin Herrmann <martin@famherrmann.de>
2012-03-18 11:00:25 +0000

    Retranslation
    git-svn-id: svn://svn.code.sf.net/p/startkladde/code/branches/i18n@1137 ffb59c96-f4eb-4fd4-83d4-e68da7e46744

--- src/plugins/info/metar/MetarPluginSettingsPane.cpp
+++ src/plugins/info/metar/MetarPluginSettingsPane.cpp
@@ -4,6 +4,13 @@
 #include "src/gui/dialogs.h"
 #include "src/plugins/info/metar/MetarPlugin.h"
 
+/*
+ * TODO:
+ *   - when retranslating, the airport input may not be drawn correctly until it
+ *     is focused. A spacer between the airport input and the label to the right
+ *     of it does not help.
+ */
+
 MetarPluginSettingsPane::MetarPluginSettingsPane (MetarPlugin *plugin, QWidget *parent):
 	PluginSettingsPane (parent),
 	plugin (plugin)
