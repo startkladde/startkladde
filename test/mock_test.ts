0fc4cea9a1d8cfb948cf57db4dad8a3d8318bf23
Martin Herrmann <martin@famherrmann.de>
2012-03-31 15:18:32 +0000

    Add test input for mock script
    git-svn-id: svn://svn.code.sf.net/p/startkladde/code/branches/i18n@1159 ffb59c96-f4eb-4fd4-83d4-e68da7e46744

---
+++ test/mock_test.ts
@@ -0,0 +1,67 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="de_DE">
+<context>
+    <name>TestContext</name>
+
+    <message>
+        <location filename="../src/dummy.cpp" line="42"/>
+        <source>Regular message</source>
+        <comment>With regular comment</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Regular message (mock translated)</source>
+		<translation type="unfinished">Mock translation</translation>
+    </message>
+    <message>
+        <source>Regular message (manually translated)</source>
+		<translation>Manual translation</translation>
+    </message>
+
+	<message>
+        <source>Multi
+line
+message</source>
+        <translation type="unfinished"></translation>
+    </message>
+	<message>
+		<source>Multi line
+message
+(mock translated)</source>
+		<translation type="unfinished">Multi line
+mock
+translation</translation>
+    </message>
+	<message>
+		<source>Multi line
+message
+(manually translated)</source>
+		<translation>Multi line
+manual
+translation</translation>
+    </message>
+
+	<message numerus="yes">
+        <source>%n numerus message(s)</source>
+        <translation type="unfinished">
+            <numerusform></numerusform>
+        </translation>
+    </message>
+    <message numerus="yes">
+        <source>%n numerus message(s) (mock translated)</source>
+        <translation type="unfinished">
+            <numerusform>%n mock translation</numerusform>
+            <numerusform>%n mock translation(s)</numerusform>
+        </translation>
+    </message>
+    <message numerus="yes">
+        <source>Numerus message(s) (manually translated)</source>
+		<translation>
+            <numerusform>%n manual translation</numerusform>
+            <numerusform>%n manual translation(s)</numerusform>
+        </translation>
+    </message>
+
+</context>
+</TS>
