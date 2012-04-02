b843da2f1820e3fde41130e4a428dd3a093550f2
Martin Herrmann <martin@famherrmann.de>
2012-04-02 16:32:08 +0000

    Update TODOs
    git-svn-id: svn://svn.code.sf.net/p/startkladde/code/branches/i18n@1170 ffb59c96-f4eb-4fd4-83d4-e68da7e46744

--- src/i18n/notr.h
+++ src/i18n/notr.h
@@ -5,4 +5,7 @@
 #define qnotr(value) QString (value)
 #define qnotrUtf8(value) QString::fromUtf8 (value)
 
+// TODO There should be qnotrCount(count, singular, plural[, none]) function,
+// but scripts/find_missing_tr does not handle multi-argument functions
+
 #endif
