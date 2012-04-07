367b52effdcca9617565033d290d5b9fd7a3236f
Martin Herrmann <martin@famherrmann.de>
2012-04-07 15:30:47 +0000

    Cleanup and translate legacy source code to english
    git-svn-id: svn://svn.code.sf.net/p/startkladde/code/trunk/startkladde@1199 ffb59c96-f4eb-4fd4-83d4-e68da7e46744

--- src/flightColor.cpp
+++ src/flightColor.cpp
@@ -1,98 +1,99 @@
 #include "src/flightColor.h"
 
+// Conditions. These macros expand to an expression.
+#define isTrue(  x) ((x)==true)
+#define isFalse( x) ((x)==false)
+#define dontCare(x) (true)
+#define isLocal(  x) ((x)==Flight::modeLocal  )
+#define isComing( x) ((x)==Flight::modeComing )
+#define isLeaving(x) ((x)==Flight::modeLeaving)
 
-// Ganz tolle Präprozessor-Macros für das Ankreuzfeld-System, siehe
-// flightColor ().
-#define ist_true(x) ((x)==true)
-#define ist_false(x) ((x)==false)
-#define ist_egal(x) (true)
-#define ist_lokal(x) ((x)==Flight::modeLocal)
-#define ist_kommt(x) ((x)==Flight::modeComing)
-#define ist_geht(x) ((x)==Flight::modeLeaving)
-#define farbe(mod, sch, sta, lan, feh, far)	\
-	if (mod(modus) && sch(schlepp) && sta(gestartet) && lan(gelandet) && feh(fehler)) return QColor (far);
-#define fargb(mod, sch, sta, lan, feh, rot, gru, bla)	\
-	if (mod(modus) && sch(schlepp) && sta(gestartet) && lan(gelandet) && feh(fehler)) return QColor (rot, gru, bla);
-#define t ist_true
-#define f ist_false
-#define x ist_egal
-#define lo ist_lokal
-#define ko ist_kommt
-#define ge ist_geht
-#define xx ist_egal
+// Indirectors. These macros expand to names of macros taking one argument.
+#define t isTrue
+#define f isFalse
+#define x dontCare
+#define local   isLocal
+#define coming  isComing
+#define leaving isLeaving
 
-#define rot 255,0,0
-#define hellrot 255,127,127
-#define blau 127,127,255
-#define hellblau 191,191,255
-#define schleppblau 162,162,216
-#define gruen 0,255,0
-#define hellgruen 191,255,191
-#define gelb 255,255,0
-#define hellgelb 255,255,191
-#define pink 255,255,0
-#define white 255,255,255
+// Generates a condition
+#define farbe(modeCondition, towflightCondition, departedCondition, landedCondition, errorCondition, resultingColor) \
+	if ( \
+		modeCondition     (mode     ) && \
+		towflightCondition(towflight) && \
+		departedCondition (departed ) && \
+		landedCondition   (landed   ) && \
+		errorCondition    (error    ))   \
+		return (resultingColor);
+
+// Colors
+#define red         QColor (255,  0,  0)
+#define lightRed    QColor (255,127,127)
+#define blue        QColor (127,127,255)
+#define lightBlue   QColor (191,191,255)
+#define towBlue     QColor (162,162,216)
+#define green       QColor (  0,255,  0)
+#define lightGreen  QColor (191,255,191)
+#define yellow      QColor (255,255,  0)
+#define lightYellow QColor (255,255,191)
+#define magenta     QColor (255,255,  0)
+#define white       QColor (255,255,255)
+
 
 // TODO move to flight
-QColor flightColor (Flight::Mode modus, bool fehler, bool schlepp, bool gestartet, bool gelandet)
-	/*
-	 * Finds out which color to use for a given flight.
-	 * Parameters:
-	 *   - mode: the mode of the flight.
-	 *   - fehler: whether the flight is erroneous.
-	 *   - schlepp: whether the flight is a towflight
-	 *   - gestarted: whether the flight has departed
-	 *   - landed: whether the flight has landed.
-	 * Return value:
-	 *   - the color for the flight.
-	 */
+/**
+ * Determines the color to use for a given flight.
+ *
+ * @param mode the mode of the flight
+ * @param error whether the flight is erroneous
+ * @param towflight whether the flight is a towflight
+ * @param departed whether the flight has departed
+ * @param landed whether the flight has landed
+ * @return the color for the flight as QColor
+ */
+QColor flightColor (Flight::Mode mode, bool error, bool towflight, bool departed, bool landed)
 {
-	// Ganz tolles Farbauswahl-Ankreuzfeld-C-Präprozessor-System, gebaut von
-	// Martin, dem Helden der Softwareentwicklung für die Luftfahrt.
-	// Zweck der Aktion ist, dass man erkennen kann, dass jeder mögliche Fall
-	// behandelt ist.
-	// Man trage in die entsprechende Spalte ein, wofür diese Farbe gelten
-	// soll. t: die entsprechende Variable muss true sein, f: die
-	// entsprechende Variable muss false sein, x: der Wert der Variablen ist
-	// egal. Der erste Treffer gewinnt.
-	// Bei Modus: "ge": Modus muss "geht" sein; "ko": ..."kommt"...; "lo":
-	// ..."lokal"...; "xx": Modus ist egal
-	//         S
-	//         c        F
-	//     M   h  S     e
-	//     o   l  t  L  h
-	//     d   e  a  a  l
-	//     u   p  r  n  e
-	//     s   p  t  d  r
+	// The purpose of this contraption is to be able to verify that each case
+	// has been handled.
+	// The columns contain the respective indirector macros - for the mode
+	// column, one of dontCare, local, coming or leaving; for the boolean
+	// columns one of t (the variable must be true), f (the variable must be
+	// false) or x (the variable is ignored). The first matching condition is
+	// used.
 
-	// Fehlerhafte Flüge
-	farbe (xx, t, x, x, t, hellrot);	// Schleppflug Fehler
-	farbe (xx, f, x, x, t, hellrot);	// Fehler
-	farbe (ge, x, f, t, x, pink);		// }Gelandet, aber nicht gestartet: Programmfehler
-	farbe (lo, x, f, t, x, pink);		// }muss vorher gecheckt werden und Fehler auslösen
+	//     Mode      Towflight
+	//                  Departed
+	//                     Landed
+	//                        Erroneous
+	//                           Color
+	//
+	// Erroneous flights
+	farbe (dontCare, t, x, x, t, lightRed); // Erroneous towflight
+	farbe (dontCare, f, x, x, t, lightRed); // Erroneous flight
 
-	// Flüge von extern
-	farbe (ko, f, x, f, f, hellblau);	// Angekündigt==Fliegt
-	farbe (ko, f, x, t, f, hellgruen);	// Gelandet
-	farbe (ko, t, x, x, f, pink);		// Programmfehler: es gibt keine kommenden Schlepps
+	// Program errors - if a flight has landed, but not departed, the error
+	// flag must be true
+	farbe (leaving , x, f, t, x, magenta ); // Leaving flight
+	farbe (local   , x, f, t, x, magenta ); // Local flight
 
-	// Flüge nach extern
-	farbe (ge, f, f, x, f, hellgelb);	// Vorbereitet
-	farbe (ge, f, t, x, f, hellgruen);	// Landung wird nicht aufgezeichnet
-	farbe (ge, t, f, x, f, gelb);		// Schlepp, Vorbereitet, kommt nicht vor
-	farbe (ge, t, t, x, f, hellgruen);	// Schlepp, Landung wird nicht aufgezeichnet
+	// Coming flights
+	farbe (coming, f, x, f, f, lightBlue ); // Flying
+	farbe (coming, f, x, t, f, lightGreen); // Landed
+	farbe (coming, t, x, x, f, magenta   ); // Program error - coming towflights are not valid
 
-	// Lokale Flüge
-	farbe (lo, f, f, x, f, hellgelb);	// nicht gestartet
-	farbe (lo, f, t, f, f, hellblau);	// Flug in der Luft
-	farbe (lo, f, t, t, f, hellgruen);	// gelandeter Flug
-	farbe (lo, t, f, x, f, gelb);		// nicht gestarteter Schleppflug (gibt es nicht)
-	farbe (lo, t, t, f, f, schleppblau);// Schleppflug in der Luft
-	farbe (lo, t, t, t, f, hellgruen);	// gelandeter Schleppflug
+	// Leaving flights
+	farbe (leaving, f, f, x, f, lightYellow); // Prepared
+	farbe (leaving, f, t, x, f, lightGreen ); // Leaving (landing is not recorded)
+	farbe (leaving, t, f, x, f, yellow     ); // Prepared towflight (cannot happen)
+	farbe (leaving, t, t, x, f, lightGreen ); // Leaving towflight
 
-//	Template
-//	farbe (ll, x, x, x, x, white);
-//	fargb (ll, x, x, x, x, 000, 000, 000);
+	// Local flights
+	farbe (local, f, f, x, f, lightYellow); // nicht gestartet
+	farbe (local, f, t, f, f, lightBlue  ); // Flug in der Luft
+	farbe (local, f, t, t, f, lightGreen ); // gelandeter Flug
+	farbe (local, t, f, x, f, yellow     ); // nicht gestarteter Schleppflug (gibt es nicht)
+	farbe (local, t, t, f, f, towBlue    ); // Schleppflug in der Luft
+	farbe (local, t, t, t, f, lightGreen ); // gelandeter Schleppflug
 
 	return QColor (255, 255, 255);	// Default: weiß
 }
