cd9b86bde480c47804fac236fbf7bfb733aff3d4
Martin Herrmann <martin@famherrmann.de>
2012-04-12 20:54:08 +0000

    Fix autogenerate_files on Windows
    git-svn-id: svn://svn.code.sf.net/p/startkladde/code/trunk/startkladde@1245 ffb59c96-f4eb-4fd4-83d4-e68da7e46744

--- script/build/autogenerate_files.rb
+++ script/build/autogenerate_files.rb
@@ -4,7 +4,11 @@ require 'pathname'
 
 dir=Pathname.new(File.dirname(__FILE__))
 
-# Use expand_path so we get an absolute path, because on Unix systems, the
-# current directory is not included in the search path
-system dir.join('generate_version_h.rb').expand_path
-system dir.join('generate_current_schema_cpp.rb').expand_path
+# Don't use system:
+#   * on Unixes, we have to use the absolute path
+#   * on Windowses, we have to invoke the interpreter explicityly, but it may
+#     not be on the path or may have a different name
+# We still have to use the absolute path, though, because it doesen't work on
+# Windows otherwise.
+require dir.join('generate_version_h.rb').expand_path.to_s
+require dir.join('generate_current_schema_cpp.rb').expand_path.to_s
