95b2349345da97c18f5903ca0e09c8643ba2fd42
Martin Herrmann <martin@famherrmann.de>
2012-04-12 12:35:46 +0000

    Improve file autogeneration
    git-svn-id: svn://svn.code.sf.net/p/startkladde/code/trunk/startkladde@1243 ffb59c96-f4eb-4fd4-83d4-e68da7e46744

--- script/build/autogenerate_files.rb
+++ script/build/autogenerate_files.rb
@@ -2,28 +2,9 @@
 
 require 'pathname'
 
-### Change to the project base directory 
+dir=Pathname.new(File.dirname(__FILE__))
 
-# This script is assumed to be located in script/build
-Dir.chdir Pathname.new(File.dirname(__FILE__)).join("..", "..")
-
-
-### Generate the version
-
-# Call the make_version script in the version directory
-Dir.chdir("version") {
-	system "ruby make_version.rb"
-}
-
-### Generate the current schema
- 
-template=Pathname.pwd.join('src', 'db', 'schema', 'CurrentSchema.cpp.erb')
-output  =Pathname.pwd.join('build', 'CurrentSchema.cpp')
-
-# Make sure the directory for the output exists
-output.dirname.mkpath
-if !system("erb -T 1 #{template} >#{output}")
-	# Generation failed. Delete the output so we don't leave an old version behind
-	puts "Generation of #{output} failed"
-	output.delete
-end
+# Use expand_path so we get an absolute path, because on Unix systems, the
+# current directory is not included in the search path
+system dir.join('generate_version_h.rb').expand_path
+system dir.join('generate_current_schema_cpp.rb').expand_path
---
+++ script/build/generate_version_h.rb
@@ -0,0 +1,40 @@
+#!/usr/bin/env ruby
+
+require 'pathname'
+require 'date'
+
+# Project base directory (this script is assumed to be in script/build)
+base_dir=Pathname.new(File.dirname(__FILE__)).join("..", "..")
+
+# File names
+version_file=base_dir.join('version', 'version')
+output      =base_dir.join('version', 'version.h')
+
+def svn_version(dir)
+  if File.directory?('.svn')
+		# This should be portable, so we can't use 'which' or redirect stderr
+		# (both won't work on Windows).
+		if File.exist?('/usr/bin/svnversion')
+			svn_version=`svnversion #{dir}`.chomp
+			"-#{svn_version}"
+		else
+			"-svn"
+		end
+	else
+		""
+	end
+end
+
+version=IO.readlines(version_file)[0].chomp
+date=Date.today.to_s
+svn=svn_version(base_dir)
+
+version_string="#{version}#{svn} (#{date})"
+
+puts "Writing #{output}"
+puts "Version is #{version_string}"
+
+output.dirname.mkpath
+File.open(output, "w") { |file|
+	file.puts "#define VERSION \"Version #{version_string}\""
+}
