fdb6951a4ef6578071df792074316c2cab0dc29c
Martin Herrmann <martin@famherrmann.de>
2012-04-23 10:13:53 +0000

    Add build type to version information
    git-svn-id: svn://svn.code.sf.net/p/startkladde/code/branches/cmake@1258 ffb59c96-f4eb-4fd4-83d4-e68da7e46744

--- src/version.cpp
+++ src/version.cpp
@@ -1,7 +1,5 @@
 #include "version.h"
 
-
-//#include "build/version.h"
 #include "text.h"
 #include "i18n/notr.h"
 #include "build.h"
@@ -10,8 +8,14 @@ QString getVersion ()
 {
 	// TODO add date, svn revision and build type, e. g.:
 	// Version 2.1.0-svn (2012-04-20)
-	return qnotr ("Version %1.%2.%3")
+	QString version=qnotr ("Version %1.%2.%3")
 		.arg (BUILD_VERSION_MAJOR)
 		.arg (BUILD_VERSION_MINOR)
 		.arg (BUILD_VERSION_REVISION);
+
+	QString buildType (BUILD_TYPE);
+	if (!buildType.isEmpty ())
+		version+=qnotr (" (build type: %1)").arg (buildType);
+
+	return version;
 }
