f9c36fdf584cd5b9becef3d74f1d6396f0105328
Martin Herrmann <martin@famherrmann.de>
2012-04-24 21:51:18 +0000

    Handle no SVN working copy Clean up messages
    git-svn-id: svn://svn.code.sf.net/p/startkladde/code/branches/cmake@1264 ffb59c96-f4eb-4fd4-83d4-e68da7e46744

--- cmake/generateSvnHeader.cmake
+++ cmake/generateSvnHeader.cmake
@@ -1,27 +1,26 @@
-include (FindSubversion)
+SET (svn_dir "${SOURCE_DIR}/.svn")
 
-if (SUBVERSION_FOUND)
-	# Extract working copy information into SVN_... variables
-	Subversion_WC_INFO (${SOURCE_DIR} SVN)
-	SET (SVN_REVISION "${SVN_WC_REVISION}")
-else ()
-	# Set dummy variables
-	SET (SVN_REVISION "svn")
-endif ()
-
+IF(EXISTS "${svn_dir}" AND IS_DIRECTORY "${svn_dir}")
+	# Looks like this is an SVN directory. Try to find SVN.
+	FIND_PACKAGE (Subversion)
+	
+	if (SUBVERSION_FOUND)
+		# Extract working copy information into SVN_... variables
+		Subversion_WC_INFO (${SOURCE_DIR} SVN)
+		SET (SVN_REVISION "${SVN_WC_REVISION}")
+		MESSAGE (STATUS "SVN working copy, revision is ${SVN_REVISION}")
+	else ()
+		# Set dummy variables
+		SET (SVN_REVISION "?")
+		MESSAGE (STATUS "SVN working copy, unknown revision (svn not found)")
+	endif ()
+ELSE ()
+	SET (SVN_REVISION "")
+	MESSAGE (STATUS "Not an SVN working copy")
+ENDIF ()
 
 # Write the header file contents to a temporary file
-file (WRITE svnVersion.h.txt "#define SVN_VERSION \"${SVN_REVISION}\"\n")
+file (WRITE svnVersion.h.tmp "#define SVN_VERSION \"${SVN_REVISION}\"\n")
 
 # Copy the file to the final file (only if it changed)
-execute_process (COMMAND ${CMAKE_COMMAND} -E copy_if_different svnVersion.h.txt svnVersion.h)
-
-                        
-# From FindSubversion.cmake:                        
-#  FIND_PACKAGE(Subversion)
-#  IF(SUBVERSION_FOUND)
-#    Subversion_WC_INFO(${PROJECT_SOURCE_DIR} Project)
-#    MESSAGE("Current revision is ${Project_WC_REVISION}")
-#    Subversion_WC_LOG(${PROJECT_SOURCE_DIR} Project)
-#    MESSAGE("Last changed log is ${Project_LAST_CHANGED_LOG}")
-#  ENDIF(SUBVERSION_FOUND)
+execute_process (COMMAND ${CMAKE_COMMAND} -E copy_if_different svnVersion.h.tmp svnVersion.h)
