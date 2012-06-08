fca0d3e3f3f84516dc5399fbb86cfd85af603dcd
Martin Herrmann <martin@famherrmann.de>
2012-06-08 21:20:56 +0000

    Better indication of SVN revision in version
    git-svn-id: svn://svn.code.sf.net/p/startkladde/code/branches/cmake@1287 ffb59c96-f4eb-4fd4-83d4-e68da7e46744

--- cmake/generateSvnHeader.cmake
+++ cmake/generateSvnHeader.cmake
@@ -6,26 +6,39 @@
 SET (svn_dir "${SOURCE_DIR}/.svn")
 
 IF(EXISTS "${svn_dir}" AND IS_DIRECTORY "${svn_dir}")
+	SET (SVN_IS_WC "true")
+	
 	# Looks like this is an SVN directory. Try to find SVN.
 	FIND_PACKAGE (Subversion)
 	
 	if (SUBVERSION_FOUND)
 		# Extract working copy information into SVN_... variables
 		Subversion_WC_INFO (${SOURCE_DIR} SVN)
-		SET (SVN_REVISION "${SVN_WC_REVISION}")
+
 		MESSAGE (STATUS "SVN working copy, revision is ${SVN_REVISION}")
+
+		SET (SVN_REVISION_KNOWN "true"              )
+		SET (SVN_REVISION       "${SVN_WC_REVISION}")
 	else ()
-		# Set dummy variables
-		SET (SVN_REVISION "?")
 		MESSAGE (STATUS "SVN working copy, unknown revision (svn not found)")
+
+		SET (SVN_REVISION_KNOWN "false")
+		SET (SVN_REVISION       ""     )
 	endif ()
 ELSE ()
-	SET (SVN_REVISION "")
 	MESSAGE (STATUS "Not an SVN working copy")
+
+	SET (SVN_IS_WC          "false")
+	SET (SVN_REVISION_KNOWN "false")
+	SET (SVN_REVISION       ""     )
 ENDIF ()
 
 # Write the header file contents to a temporary file
-file (WRITE svnVersion.h.tmp "#define SVN_VERSION \"${SVN_REVISION}\"\n")
+file (WRITE svnVersion.h.tmp
+	"#define SVN_IS_WC            ${SVN_IS_WC}         \n"
+	"#define SVN_REVISION_KNOWN   ${SVN_REVISION_KNOWN}\n"
+	"#define SVN_REVISION       \"${SVN_REVISION}\"    \n"
+)
 
 # Copy the file to the final file (only if it changed)
 execute_process (COMMAND ${CMAKE_COMMAND} -E copy_if_different svnVersion.h.tmp svnVersion.h)
--- src/version.cpp
+++ src/version.cpp
@@ -7,22 +7,37 @@
 #include "build.h"
 #include "svnVersion.h"
 
+/**
+ * Returns a descriptive version string intended for display to the user
+ *
+ * The string contains the version, which is similar to:
+ *   - 2.1.3       if not built from an SVN working copy
+ *   - 2.1.3-r1234 if built from an SVN working copy with known revision
+ *   - 2.1.3-svn   if built from an SVN working copy with unknown revision
+ *
+ * Also included in the version string is the build type
+ */
 QString getVersion ()
 {
-	// TODO add date, svn revision and build type, e. g.:
-	// Version 2.1.0-svn (2012-04-20)
 	QString version=qnotr ("Version %1.%2.%3")
 		.arg (BUILD_VERSION_MAJOR)
 		.arg (BUILD_VERSION_MINOR)
 		.arg (BUILD_VERSION_REVISION);
 
-	QString svnInfo (SVN_VERSION);
-	if (!svnInfo.isEmpty ())
-		version+=qnotr (" revision %1").arg (svnInfo);
+	if (SVN_IS_WC)
+	{
+		// The program is being built from an SVN working copy. We may or may
+		// not know the SVN revision, depending on whether the SVN client was
+		// found.
+		if (SVN_REVISION_KNOWN)
+			version+=qnotr ("-r%1").arg (SVN_REVISION);
+		else
+			version+=qnotr ("-svn");
+	}
 
 	QString buildType (BUILD_TYPE);
 	if (!buildType.isEmpty ())
-		version+=qnotr (" (build type: %1)").arg (buildType);
+		version+=qnotr (" (build type: %1)").arg (firstToLower (buildType));
 
 	return version;
 }
