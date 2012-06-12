7b12b1783354e31ed710ba5d8f75a689fb0d8085
Martin Herrmann <martin@famherrmann.de>
2012-06-12 13:45:15 +0000

    Update migration/schema generation for CMake build system
    git-svn-id: svn://svn.code.sf.net/p/startkladde/code/branches/flarm@1310 ffb59c96-f4eb-4fd4-83d4-e68da7e46744

--- script/generate_migration.rb
+++ script/generate_migration.rb
@@ -20,7 +20,7 @@ end
 
 if name.empty?
 	puts "Usage: #{$0} [-f|--force] [-t|--timestamp timestamp] name"
-	puts "  name: the name of the migration, without the version"
+	puts "  name: the name of the migration, without the version - use underscore_notation"
 	puts "  timestamp: 14 digits; the current time is used if not specified"
 	puts "Example: #{$0} add_towpilot"
 	exit 1
@@ -75,7 +75,6 @@ write source_template, source
 write header_template, header
 
 puts "All done. You will now have to implement the migration and then call"
-puts "'./script/update_current_schema.sh' with a configured and cleared"
-puts "database to update the schema definition."
-
-
+puts "'../path_to_source_directory/script/update_current_schema.sh' from the"
+puts "build directory, with a configured and cleared database, to update the"
+puts "schema definition."
