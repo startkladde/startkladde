84cf80d7542dffd85707837a6c49384e4d5f70bb
deffi <deffi@ffb59c96-f4eb-4fd4-83d4-e68da7e46744>
2012-06-20 08:02:04 +0000

    More test cases
    git-svn-id: svn://svn.code.sf.net/p/startkladde/code/trunk/startkladde@1421 ffb59c96-f4eb-4fd4-83d4-e68da7e46744

--- script/find_missing_tr2.rb
+++ script/find_missing_tr2.rb
@@ -3,23 +3,7 @@
 require 'rubygems'
 require 'rainbow'
 
-#	test_cases =<<-EOF
-#	FUNCTION ();
-#
-#	FUNCTION (); FUNCTION (); // COMMENT
-#	FUNCTION (); /* COMMENT */ FUNCTION ();
-#	FUNCTION ("STRING");
-#
-#	FUNCTION ("before // not a comment, a STRING");
-#	FUNCTION ("before /* not a comment, a STRING */ after");
-#
-#	FUNCTION (); // before "not a string, a COMMENT"
-#	FUNCTION (); /* before "not a string, a COMMENT" after */ FUNCTION ();
-#	EOF
 
-# Test cases: 
-#   line comment with line continuation 
-#   /* ... */// foo
 
 
 def scan_cpp(text)
@@ -82,11 +66,14 @@ def scan_cpp(text)
 			else raise "Unhandled state #{state.inspect}"
 			end
 
-			next_segment_start=0 if !next_state
+			if (next_segment_start!=segment_start)
+				next_segment_start=0 if !next_state
 
-			segment=text[segment_start..next_segment_start-1]
-			puts "#{segment_start}-#{next_segment_start-1} - #{state.inspect} - #{segment.inspect}"
-			colored += segment.color(colors[state])
+				segment=text[segment_start..next_segment_start-1]
+				puts "#{segment_start}-#{next_segment_start-1} - #{state.inspect} - #{segment.inspect}"
+				colored += segment.color(colors[state])
+			end
+
 			segment_start=next_segment_start
 			state=next_state
 		end
@@ -99,10 +86,38 @@ def scan_cpp(text)
 end
 
 text=<<EOF
-void foo ();
-a="bar"; // assignment
-b=2; /* more assignments */ return "baz";
+// Comment at the beginning
+
+void foo (const QString &x="STRING")
+{
+	// Simple cases
+	FUNCTION (); FUNCTION (); // COMMENT
+	FUNCTION (); /* COMMENT */ FUNCTION ();
+	FUNCTION ("STRING");
+
+	// Adjacent segments
+	"STRING""STRING"/* COMMENT *//* COMMENT */"STRING"// COMMENT
+
+	// String with comment delimiters
+	FUNCTION ("before // not a comment, a STRING");
+	FUNCTION ("before /* not a comment, a STRING */ after");
+
+	// Comments with string delimiters
+	FUNCTION (); // before "not a string, a COMMENT"
+	FUNCTION (); /* before "not a string, a COMMENT" after */ FUNCTION ();
+
+	// Comments with other comment delimiters
+	FUNCTION (); // /* block comment */ still a comment
+	FUNCTION (); /* block comment // still a comment */
+	FUNCTION (); /* block comment /* still a comment */
+
+	// TODO: line comment with line continuation 
+	// TODO: string with line continuation
+	// TODO: string with escaped double quote
+	// TODO: string with escaped backslash before the end
+}
 EOF
+
 
 puts text
 puts
