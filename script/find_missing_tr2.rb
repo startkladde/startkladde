f63fd0cbc36997eeb8ca52f57d0ab0f2f0b0d24c
deffi <deffi@ffb59c96-f4eb-4fd4-83d4-e68da7e46744>
2012-06-20 14:55:40 +0000

    Comments, cleanup
    git-svn-id: svn://svn.code.sf.net/p/startkladde/code/trunk/startkladde@1428 ffb59c96-f4eb-4fd4-83d4-e68da7e46744

--- script/find_missing_tr2.rb
+++ script/find_missing_tr2.rb
@@ -41,18 +41,25 @@ end
 #      * string_literal '"fred"
 #      * default        '; '
 #      * line_comment   '// A string'
-# Or at least that's what we hope.
+# This simple task is complicated by the fact that the meaning of the separator
+# characters depends on their context. For example, double quotes inside a
+# comment do not delimit a string.
 class CppSegmenter
-	attr_reader :segments, :state
+	attr_reader :state
 
 	def initialize
+		reset
+	end
+
+	def reset
 		@state=:default
 	end
 
 	def process_line(line)
-		line=line.chomp
-
 		result=[]
+
+		# We don't want to include the line breaks in the segments
+		line=line.chomp
 
 		# Start at the beginning of the line
 		segment_start=0
@@ -66,46 +73,68 @@ class CppSegmenter
 			# By default, we stay in the current state and don't skip anything.
 			next_state=@state; next_segment_start=nil; skip=0
 
-#			# Find the next separator. Possible separators depend on the state
-#			# we're currently in.
-#			# We set the following variables:
-#			#   * next_state: the state associated with the next part
-#			#   * next_part_start: the starting position of the next part, or
-#			#     nil if there is no next part in this line (this probably
-#			#     means that the part is continued in the next line)
-#			#   * skip: the number of characters to skip, starting at
-#			#     next_part_start. This is the length of the separator if the
-#			#     separator belongs to the next part, or 0 if the separator
-#			#     belongs to the current part.
+			# Find the next separator. Possible separators depend on the state
+			# we're currently in. For example, inside a string literal, "//" is
+			# not a separator.
+			# We set the following variables:
+			#   * next_state: the state associated with the next segment
+			#   * next_segment_start: the starting position of the next
+			#     segment, or nil if the current segment extends to the end of
+			#     the line (and possibly beyond)
+			#   * skip: the number of characters to skip, starting at
+			#     next_segment_start. This is the length of the separator if
+			#     the separator belongs to the next part, or 0 if the separator
+			#     belongs to the current part.
 			case @state
 			when :default
-				# Look for strings that can start a new segment
-				next_segment_start=line.index(/ (") | (\/\/) | (\/\*) /x, pos)
-				if    $1 then next_state=:string_literal; skip=1
-				elsif $2 then next_state=:line_comment  ; skip=2
-				elsif $3 then next_state=:block_comment ; skip=2
+				# Look for the first string that can start a new segment
+				# (separator): a double quote starts a string literal, two
+				# slashes start a line comment and a slash followed by an
+				# asterisk starts a block comment. Go to the appropriate state
+				# and skip the separator.
+				# If no separator is found, stay in the current state and
+				# next_segment_start will be left at nil to indicate that the
+				# segment extends to the end of the line.
+				pos=line.index(/ (") | (\/\/) | (\/\*) /x, pos)
+				if    $1 then next_state=:string_literal; next_segment_start=pos; skip=1
+				elsif $2 then next_state=:line_comment  ; next_segment_start=pos; skip=2
+				elsif $3 then next_state=:block_comment ; next_segment_start=pos; skip=2
 				end
 			when :string_literal
-				# Look for the end of the string literal. Skip over escaped
-				# backslashes and escaped quotes.
+				# Look for the end of the string literal (a double quote), an
+				# escaped backslash or an escaped quote. If an escaped
+				# backslash or an escaped quote is encountered, it is skipped
+				# and the process is repeated.
+				# If no end of the string literal is found, stay in the current
+				# state and next_segment_start will be left at nil to indicate
+				# that the segment extends to the end of the line.
 				begin
-					pos=line.index(/ (") | (\\\" | \\\\) /x, pos)
+					pos=line.index(/ (") | (\\\") | (\\\\) /x, pos)
 					if    $1 then next_state=:default; next_segment_start=pos+1; skip=0
-					elsif $2 then pos+=2 # Skip over the escaped backslash or quote
+					elsif $2 then pos+=2 # Skip over the escaped quote
+					elsif $3 then pos+=2 # Skip over the escaped backslash
 					end
 				end until next_state!=@state || !pos
 			when :block_comment
-				# Look for the end of the block comment
+				# Look for the end of the block comment (an asterisk followed
+				# by a slash).
+				# If no end of the block comment is found, stay in the current
+				# state and next_segment_start will be left at nil to indicate
+				# that the segment extends to the end of the line.
 				pos=line.index(/ (\*\/) /x, pos)
 				if $1 then next_state=:default; next_segment_start=pos+2; skip=0
 				end
 			when :line_comment
-				# Look for the end of the line
+				# Look for the end of the line comment (the end of the line).
+				# If a backslash preceds the end of the line, this is not
+				# counted as the end of the line comment. In this case, the
+				# state is not changed.
+				# In any case, next_segment_start is left at nil to indicate
+				# that the segment extends to the end of the line.
 				pos=line.index(/ (\\$) | ($) /x, pos)
 				if    $1 then 
 				elsif $2 then next_state=:default
 				end
-				next_segment_start=nil
 			else raise "Unhandled state #{@state.inspect}"
 			end
 
@@ -209,8 +238,13 @@ def pretty_print(lines)
 		:block_comment  => :green,
 		:line_comment   => :blue
 	}
-	colored=""
-	lines.each { |segments|
+
+	max_line_num=lines.size+1
+	padlength=max_line_num.to_s.length
+
+	lines.each_with_index { |segments, index|
+		line_number=index+1
+		print "#{line_number.to_s.rjust(padlength)}:"
 		segments.each { |segment|
 			print segment.code.color(colors[segment.type])
 		}
