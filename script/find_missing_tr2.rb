8b65a9dc0a57b2417f72a9dff3c0472d0bcd1829
deffi <deffi@ffb59c96-f4eb-4fd4-83d4-e68da7e46744>
2012-06-20 22:36:41 +0000

    Character-based approach (for comparison) Attempt at generalization
    git-svn-id: svn://svn.code.sf.net/p/startkladde/code/trunk/startkladde@1435 ffb59c96-f4eb-4fd4-83d4-e68da7e46744

--- script/find_missing_tr2.rb
+++ script/find_missing_tr2.rb
@@ -65,6 +65,14 @@ class CppSegmenter
 		segment_start=0
 		skip=0
 
+		# FIXME: can we simplify this by
+		#   * having more states and assigning the type explicitly
+		#     => create new segment when type changes
+		#     => maybe this gets rid of next_segment_start and skip
+		#   * taking away the processed part of the line?
+		# Note that all input characters should be output again, no
+		# transformation takes place, we only assign the segment type
+
 		# As long as there is a segment, we have to process it
 		while segment_start
 			# We may have to skip some characters
@@ -170,6 +178,138 @@ class CppSegmenter
 	end
 end
 
+# This one is based on characters instead of regular expressions
+class CppSegmenter2
+	attr_reader :state
+
+	def initialize
+		reset
+	end
+
+	def reset
+		@state=:default
+	end
+
+
+	def process_line(line)
+		result=[]
+
+		# We don't want to include the line breaks in the segments
+		line=line.chomp
+
+		code=""
+
+		segment_finished=lambda { |type|
+			result << CodeSegment.new(type, code)
+			code=""
+		}
+
+		# FIXME can we find a generic description for the state machine?
+		#   * state, input => new state, output type
+		#   * create new segment whenever the output type changes
+		#   * what about "special" output? => goes to buffer when output type==nil?
+		#   * what about EOL? => :eol input value?
+		# FIXME which one is faster?
+		state_machine={
+			:default                  => {
+				'"'  => [:string_literal        , :string_literal ],
+				'/'  => [:default_slash         , nil             ],
+				nil  => [:default               , :default        ]},
+			:default_slash            => {
+				'/'  => [:line_comment          , :line_comment   ],
+				'*'  => [:block_comment         , :block_comment  ]},
+			:line_comment             => {
+				'\\' => [:line_comment_backslash, :line_comment   ]},
+			:line_comment_backslash   => {
+			},
+			:block_comment            => {
+				'*'  => [:block_comment_asterisk, :block_comment  ]
+			},
+			:block_comment_asterisk   => {
+				'/'  => [:default               , :block_comment  ],
+				nil  => [:block_comment         , :block_comment  ]},
+			:string_literal           => {
+				'"'  => [:default                 , :string_literal ],
+				'\\' => [:string_literal_backslash, nil             ]
+			},
+			:string_literal_backslash => {
+				nil => [:string_literal         , :string_literal ]
+			}
+		}
+
+		line.each_char { |c|
+			case @state
+			when :default
+				case c
+				when '"'  then @state=:string_literal          ;          segment_finished.call(:default); code+=c
+				when '/'  then @state=:default_slash
+				else                                             code+=c
+				end
+			when :default_slash
+				case c
+				when '/' then @state=:line_comment             ;           segment_finished.call(:default); code+='//'
+				when '*' then @state=:block_comment            ;           segment_finished.call(:default); code+='/*'
+				else          @state=:default                  ; code+='/'+c
+				end
+			when :line_comment
+				case c
+				when '\\' then @state=:line_comment_backslash
+				else                                             code+=c
+				end
+			when :line_comment_backslash
+				@state=:line_comment; code+='\\'+c
+			when :block_comment
+				case c
+				when '*'  then @state=:block_comment_asterisk
+				else                                             code+=c
+				end
+			when :block_comment_asterisk
+				case c
+				when '/' then @state=:default                  ; code+='*/'; segment_finished.call(:block_comment)
+				else          @state=:block_comment            ; code+=c
+				end
+			when :string_literal
+				case c
+				when '"'  then @state=:default                 ; code+=c   ; segment_finished.call(:string_literal)
+				when '\\' then @state=:string_literal_backslash
+				else                                             code+=c
+				end
+			when :string_literal_backslash
+				case c
+				when '\\' then @state=:string_literal          ; code+='\\'+c
+				when '"'  then @state=:string_literal          ; code+='\\'+c
+				else           @state=:string_literal          ; code+='\\'+c
+				end
+			else
+				raise "Unhandled state #{@state.inspect}"
+			end
+		}
+
+		# EOL
+		# FIXME call segment_finished
+		case @state
+		when :line_comment
+			@state=:default; result << CodeSegment.new(:line_comment, code); code="";
+		when :string_literal_backslash
+			@state=:string_literal; code+='\\'; result << CodeSegment.new(:string_literal, code); code="";
+		when :line_comment_backslash
+			result << CodeSegment.new(:line_comment, code); code="";
+		else
+			# FIXME this stinks => have all cases been handled?
+			# FIXME comment with line continuation => backslash in wrong line?
+			result << CodeSegment.new(@state, code); code="";
+		end
+
+		return result
+	end
+
+	def process_text(text, print)
+		text.lines.map { |line|
+			process_line(line)
+		}
+	end
+end
+
 #
 #                |   |  |            |     |                   |         |   |   index of separator element
 #                1   1  2            1     2                   2         1   1
@@ -217,6 +357,7 @@ void foo (const char *x="STRING")
   "STRING\\"STRING";
   "STRING\\\\";
   "STRING";
+  // line comment with \\n in the middle
   "multi_
 line_
 string";
@@ -246,14 +387,15 @@ def pretty_print(lines)
 		line_number=index+1
 		print "#{line_number.to_s.rjust(padlength)}:"
 		segments.each { |segment|
-			print segment.code.color(colors[segment.type])
+			color=colors[segment.type]
+			print segment.code.color(color)
 		}
 		puts
 	}
 end
 
 begin
-	segmenter=CppSegmenter.new
+	segmenter=CppSegmenter2.new
 	segments=segmenter.process_text text, true
 #ensure
 	pretty_print segments
