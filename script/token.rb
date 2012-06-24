1c07d08d4a0c487572bcc7595464a22e6860f256
deffi <deffi@ffb59c96-f4eb-4fd4-83d4-e68da7e46744>
2012-06-24 21:13:36 +0000

    Documentation
    git-svn-id: svn://svn.code.sf.net/p/startkladde/code/trunk/startkladde@1448 ffb59c96-f4eb-4fd4-83d4-e68da7e46744

--- script/token.rb
+++ script/token.rb
@@ -1,3 +1,6 @@
+# A token, consisting of
+#   * a type (a symbol)
+#   * the code (a string)
 class Token
 	attr_accessor :type
 	attr_accessor :code
@@ -7,6 +10,13 @@ class Token
 		@code=code
 	end
 
+	# Determines whether a token matches a pattern.
+	# 
+	# Parameters:
+	#   * type: must match the token's type
+	#   * code: can be a string (must be identical to the token's code), a
+	#           regexp (must match the token's code) or nil (the token's code
+	#           is ignored)
 	def match?(type, code=nil)
 		if code.nil?
 			@type==type
