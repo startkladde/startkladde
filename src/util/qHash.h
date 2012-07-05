f2a344883b5989311a06a943c25d4903b9e00558
Martin Herrmann <martin@famherrmann.de>
2012-07-05 20:14:57 +0000

    Add QHash utility functions
    git-svn-id: svn://svn.code.sf.net/p/startkladde/code/branches/flarm@1503 ffb59c96-f4eb-4fd4-83d4-e68da7e46744

---
+++ src/util/qHash.h
@@ -0,0 +1,32 @@
+#ifndef QHASH_H_
+#define QHASH_H_
+
+/**
+ * Clears a QHash whose values are pointers and deletes all pointers
+ */
+template<class K, class V> void clearAndDelete (QHash<K, V *> &hash)
+{
+	foreach (V *value, hash)
+		delete value;
+
+	hash.clear ();
+}
+
+/**
+ * Removes a value from the hash whose values are pointers and deletes it
+ *
+ * If the item does not exist in the hash, nothing happens. If there are
+ * multiple items for the key in the hash, FIXME
+ *
+ */
+template<class K, class V> void removeAndDeleteIfExists (QHash<K, V *> &hash, const K &key)
+{
+	V *value=hash.value (key);
+	if (value)
+	{
+		hash.remove (key);
+		delete value;
+	}
+}
+
+#endif
