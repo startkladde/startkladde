61c0698fbfb2c8035cfff03a2d3f7b89a16ed8eb
Eggert Ehmke <eggert.ehmke@berlin.de>
2012-07-19 21:46:19 +0000

    added flarmnet table and functions
    git-svn-id: svn://svn.code.sf.net/p/startkladde/code/branches/flarm@1544 ffb59c96-f4eb-4fd4-83d4-e68da7e46744

---
+++ src/db/migrations/Migration_20120620223948_add_flarmnet.cpp
@@ -0,0 +1,29 @@
+#include "Migration_20120620223948_add_flarmnet.h"
+
+REGISTER_MIGRATION (20120620223948, add_flarmnet)
+
+Migration_20120620223948_add_flarmnet::Migration_20120620223948_add_flarmnet (Interface &interface):
+	Migration (interface)
+{
+}
+
+Migration_20120620223948_add_flarmnet::~Migration_20120620223948_add_flarmnet ()
+{
+}
+
+void Migration_20120620223948_add_flarmnet::up ()
+{
+	createTable  ("flarmnet");
+	addColumn ("flarmnet", "flarm_id",     dataTypeString16 (), "NOT NULL", true);
+	addColumn ("flarmnet", "registration", dataTypeString16 (), "NOT NULL", true);
+	addColumn ("flarmnet", "callsign",     dataTypeString16 (), "", true);
+	addColumn ("flarmnet", "owner",        dataTypeString (),   "", true);
+	addColumn ("flarmnet", "type",         dataTypeString (),   "", true);
+	addColumn ("flarmnet", "frequency",    dataTypeString16 (), "", true);
+}
+
+void Migration_20120620223948_add_flarmnet::down ()
+{
+	dropTable ("flarmnet");
+}
+
