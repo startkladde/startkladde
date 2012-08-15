5c4b123377c0ca57a094fe57777ddd88d8b9a562
Martin Herrmann <martin@famherrmann.de>
2012-08-15 18:46:45 +0000

    Proper row height
    git-svn-id: svn://svn.code.sf.net/p/startkladde/code/branches/flarm@1587 ffb59c96-f4eb-4fd4-83d4-e68da7e46744

--- src/flarm/FlarmNetOverview.cpp
+++ src/flarm/FlarmNetOverview.cpp
@@ -10,19 +10,17 @@
 
 class FlarmNetRecord;
 
-// FIXME add importing (from file or web)
-
 FlarmNetOverview::FlarmNetOverview (DbManager &dbManager, QWidget *parent):
 	SkDialog<Ui::FlarmNetOverviewDialog> (parent)
 {
 	ui.setupUi (this);
 
-	//search button
-        QStyle* style = QApplication::style();
-	ui.clearButton->setIcon (style->standardIcon(QStyle::SP_DialogDiscardButton));
+	// Search button
+	QStyle* style = QApplication::style ();
+	ui.clearButton->setIcon (style->standardIcon (QStyle::SP_DialogDiscardButton));
 
-        connect (ui.searchEdit,  SIGNAL(textChanged(const QString&)), this, SLOT(searchTextChanged(const QString&)));
-        connect (ui.clearButton, SIGNAL(pressed()), this, SLOT(searchClear()));
+	connect (ui.searchEdit,  SIGNAL(textChanged(const QString&)), this, SLOT(searchTextChanged(const QString&)));
+	connect (ui.clearButton, SIGNAL(pressed()), this, SLOT(searchClear()));
 
 	// Get the list of FlarmNet records from the database. It will be deleted by
 	// the ObjectListModel.
@@ -36,6 +34,29 @@ FlarmNetOverview::FlarmNetOverview (DbManager &dbManager, QWidget *parent):
 		flarmNetRecords, true,
 		flarmNetRecordModel, true,
 		this);
+
+	// The default row height is too big. Resizing the rows to the contents
+	// (using resizeRowsToContents or setautoResizeRows) is very slow for larger
+	// lists. Therefore, we use a dummy list to determine the default row height
+	// and use setDefaultSectionSize of the vertical header to set the height
+	// for rows added later.
+	// FIXME this should be encapsulated. However, we'd like to be able to do
+	// this without explicitly knowing the type of the object, so the generic
+	// model viewer window does not have to be a template. The type is known to
+	// the model which is passed into this class, so the model should be able
+	// to return dummy contents.
+	// The functionality could also be in the view, but note that at the current
+	// time, the model may be empty, so we'd have to set a flag to consider the
+	// first item added.
+    EntityList<FlarmNetRecord> dummyList;
+    dummyList.append (FlarmNetRecord ());
+    FlarmNetRecordModel dummyObjectModel;
+    ObjectListModel<FlarmNetRecord> dummyListModel (&dummyList, false, &dummyObjectModel, false);
+    ui.flarmNetTable->setModel (&dummyListModel);
+    ui.flarmNetTable->resizeRowsToContents ();
+    int rowHeight=ui.flarmNetTable->verticalHeader ()->sectionSize (0);
+	ui.flarmNetTable->verticalHeader ()->setDefaultSectionSize (rowHeight);
+	ui.flarmNetTable->setModel (NULL);
 
 	// Create a sort/filter proxy model. It will be deleted by its parent, this.
 	proxyModel = new QSortFilterProxyModel (this);
