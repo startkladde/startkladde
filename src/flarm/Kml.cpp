82a012b99507b96248af26d82e29784c44c76691
Martin Herrmann <martin@famherrmann.de>
2012-08-17 17:47:26 +0000

    Interpret empty color as white (white placemarks may not have a color tag)
    git-svn-id: svn://svn.code.sf.net/p/startkladde/code/branches/flarm@1599 ffb59c96-f4eb-4fd4-83d4-e68da7e46744

--- src/flarm/Kml.cpp
+++ src/flarm/Kml.cpp
@@ -23,12 +23,16 @@ QPen Kml::Style::linePen ()
 // ** Functions **
 // ***************
 
-// aabbggrr
 QColor Kml::parseColor (const QString color)
 {
+	// It seems like white, fully opaque, may be stored as an empty string.
+	if (color.isEmpty ())
+		return Qt::white;
+
 	if (color.length ()!=8)
 		return QColor ();
 
+	// aabbggrr
 	int a=color.mid (0, 2).toInt (NULL, 16);
 	int b=color.mid (2, 2).toInt (NULL, 16);
 	int g=color.mid (4, 2).toInt (NULL, 16);
