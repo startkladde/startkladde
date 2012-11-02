65b3d8790df48e9a31ec061a89d10c26b6eb8c99
Martin Herrmann <martin@famherrmann.de>
2012-11-02 11:32:46 +0000

    Documentation and minor cleanup
    git-svn-id: svn://svn.code.sf.net/p/startkladde/code/branches/flarm@1672 ffb59c96-f4eb-4fd4-83d4-e68da7e46744

--- src/flarm/FlarmIdCheck.cpp
+++ src/flarm/FlarmIdCheck.cpp
@@ -4,9 +4,59 @@
 #include "src/model/Plane.h"
 #include "src/gui/windows/input/ChoiceDialog.h"
 #include "src/concurrent/monitor/OperationCanceledException.h"
+#include "src/text.h"
 
+
+// The conditions for a conflict are:
+//   * the new Flarm ID is not empty (new!="")
+//   * the other plane's Flarm ID is identical to the new Flarm ID (other==new)
+
+// The following conflict resolutions are available:
+//   * clear - other = ""
+//   * swap  - other = old
+//   * keep  - new   = old
+//   * ignore
+//   * cancel
+
+// The useful resolutions depend on the conditions: the old Flarm ID may be
+// empty or non-empty. The new Flarm ID may be the same as or different from the
+// old Flarm ID.
+//
+// Ignore and cancel are always available. Other resolutions must either change
+// new or other to remove the conflict.
+//
+// This is only relevant for conflicts: new==other, new!=""
+//
+//   Old Flarm ID | New/other Flarm ID | Comments/Resolutions
+//   -------------+--------------------+-----------------------
+//   old==""      | new==old           | No conflict (new == "")
+//                |                    |
+//   old==""      | new!=old           | Flarm ID added
+//                |                    | clear  -  other=""
+//                |                    | keep   -  new  =old  -  keep empty value
+//                |                    | (swap is the same as clear)
+//                |                    |
+//   old!=""      | new==old           | Flarm ID not changed
+//                |                    | clear  -  other=""
+//                |                    | (swap is the same as ignore)
+//                |                    | (keep is the same as ignore)
+//                |                    |
+//   old!=""      | new!=old           | Flarm ID changed
+//                |                    | clear  -  other=""
+//                |                    | swap   -  other=old
+//                |                    | keep   -  new  =old
+
+
+
+/**
+ * Creates a FlarmIdCheck
+ *
+ * @param dbManager the database to access for reading and writing
+ * @param parent the Qt parent widget for dialogs shown to the user
+ */
 FlarmIdCheck::FlarmIdCheck (DbManager &dbManager, QWidget *parent):
-	dbManager (dbManager), parent (parent)
+	dbManager (dbManager), parent (parent),
+	planeId (invalidId), selectedReaction (ignore)
 {
 }
 
@@ -14,72 +64,81 @@ FlarmIdCheck::~FlarmIdCheck ()
 {
 }
 
-QString FlarmIdCheck::Reaction_getText (Reaction reaction, const Plane &conflictingPlane,
-	const QString &newFlarmId, const QString &oldFlarmId)
-{
-	(void)newFlarmId;
-
-	switch (reaction)
-	{
-		case clear:
-			return tr ("Clear the Flarm ID of %1")
-				.arg (conflictingPlane.registration);
-		case swap:
-			return tr ("Swap the Flarm ID with %1")
-				.arg (conflictingPlane.registration);
-		case ignore:
-			return tr ("Ignore the conflict - automatic departures and "
-				"landings may not work correctly");
-		case keep:
-			// FIXME it may be empty - use a different message
-			return tr ("Keep this plane's Flarm ID at its old value, %1")
-				.arg (oldFlarmId);
-		case cancel:
-			return tr ("Cancel");
-
-		// No default
-	}
-
-	return "";
-}
-
+/**
+ * Checks the database to see if there is a Flarm ID conflict
+ *
+ * A conflicting plane is a plane that has the same Flarm ID as the Flarm ID
+ * we're trying to set (i. e. the new Flarm ID) and a different Flarm ID than
+ * the plane we're editing. The latter restriction is required because when
+ * updating an existing plane and *not* changing the Flarm ID, we still want to
+ * check for conflicts, but we will always find the same plane when looking for
+ * a plane with that Flarm ID.
+ *
+ * @return a Plane if a conflict was found, or an invalid Maybe if not
+ */
 Maybe<Plane> FlarmIdCheck::findConflictingPlane ()
 {
 	Cache &cache=dbManager.getCache ();
 
+	// If the new Flarm ID is empty, there is no conflict
+	if (isBlank (newFlarmId))
+		return NULL;
+
+	// Find a list of conflicting plane IDs. Note that this may also include
+	// the same flight (if the Flarm ID was not changed), which is not actually
+	// a conflict.
 	QList<dbId> conflictingPlaneIds=cache.getPlaneIdsByFlarmId (newFlarmId);
 
-	// Return no plane if there are no planes with that Flarm ID
+	// If there are no planes with that Flarm ID, there is no conflict.
 	if (conflictingPlaneIds.isEmpty ())
 		return NULL;
 
-	// Look at all the plane IDs with that Flarm ID
+	// If any of the planes we found is actually a conflict (i. e., not the one
+	// we're editing), retrieve it from the database and return it.
 	foreach (dbId id, conflictingPlaneIds)
 	{
-		// If the plane ID is different from the ID of the plane we're editing,
-		// we found a conflicting plane. Retrieve it from the database and
-		// return it.
+		// Make sure it is different from the plane we're editing.
 		if (id!=planeId)
 		{
-			// Retrieve the conflicting plane from the database
 			try
 			{
+				// Retrieve the plane
 				return cache.getObject<Plane> (id);
 			}
-			catch (Cache::NotFoundException &) {}
+			catch (Cache::NotFoundException &)
+			{
+				// This should never happen because we just got the ID from the
+				// database.
+			}
 		}
 	}
 
-	// We didn't find a conflicting plane.
+	// We didn't find a conflicting plane (the only plane with that Flarm ID is
+	// the plane we're editing).
 	return NULL;
 }
 
+/**
+ * Determines the possible resolutions to a conflict and suggests a default
+ *
+ * The useful resolution depend on several factors, such as whether the Flarm ID
+ * actually changed and whether the Flarm ID was set before.
+ *
+ * This method does not check if there is a conflict. Its result is only
+ * meaningful if there is.
+ *
+ * @return an Options instance, which contains a list of possible resolutions
+ *         and a default resolution
+ */
 FlarmIdCheck::Options FlarmIdCheck::getOptions ()
 {
-	// cancel is never returned. It is not offered along with the other choices
-	// but represents the user canceling the choice dialog.
+	// cancel is never returned. It is not explicitly offered along with the
+	// other resolutions but represents the user canceling the choice dialog.
 
 	Options result;
+
+	// For the resolutions available in different situations, see the comments
+	// at the beginning of the file.
 
 	if (newFlarmId!=oldFlarmId)
 	{
@@ -111,7 +170,57 @@ FlarmIdCheck::Options FlarmIdCheck::getOptions ()
 	return result;
 }
 
-FlarmIdCheck::Reaction FlarmIdCheck::showChoiceDialog (const Options &options)
+/**
+ * Creates a textual representation for a (possible) reaction for diaplying to
+ * the user. This depends heavily on the current status.
+ */
+QString FlarmIdCheck::makeText (Resolution reaction)
+{
+	(void)newFlarmId;
+
+	// No conflict
+	if (!conflictingPlane.isValid ())
+		return "";
+
+	switch (reaction)
+	{
+		case clear:
+			return tr ("Clear the Flarm ID of %1")
+				.arg (conflictingPlane->registration);
+		case swap:
+			return tr ("Swap the Flarm ID with %1")
+				.arg (conflictingPlane->registration);
+		case ignore:
+			return tr ("Ignore the conflict - automatic departures and "
+				"landings may not work correctly");
+		case keep:
+			// The old Flarm ID may be empty. In this case, we need a different
+			// message.
+			if (oldFlarmId.isEmpty ())
+				return tr ("Don't set a Flarm ID for this plane");
+			else
+				return tr ("Keep this plane's Flarm ID at its old value, %1")
+					.arg (oldFlarmId);
+		case cancel:
+			// Not actually used
+			return tr ("Cancel");
+
+		// No default
+	}
+
+	return "";
+}
+
+/**
+ * Displays a choice dialog to the user and returns the chosen resolution
+ *
+ * The available resolutions and the default resolution are passed in the
+ * options argument.
+ *
+ * If the user cancels, cancel will be returned. Otherwise, the selected
+ * resolution will be returned.
+ */
+FlarmIdCheck::Resolution FlarmIdCheck::showChoiceDialog (const Options &options)
 {
 	if (!conflictingPlane.isValid ())
 		return keep;
@@ -128,7 +237,7 @@ FlarmIdCheck::Reaction FlarmIdCheck::showChoiceDialog (const Options &options)
 
 	for (int i=0, n=options.reactions.size (); i<n; ++i)
 	{
-		choiceOptions << Reaction_getText (options.reactions[i], conflictingPlane.getValue (), newFlarmId, oldFlarmId);
+		choiceOptions << makeText (options.reactions[i]);
 
 		if (options.reactions[i]==options.defaultReaction)
 			defaultChoiceOption=i;
@@ -143,9 +252,13 @@ FlarmIdCheck::Reaction FlarmIdCheck::showChoiceDialog (const Options &options)
 }
 
 /**
+ * Checks for a conflict and asks the user for a resolution
  *
- * @param newFlarmId
- * @param oldFlarmId
+ * If there is no conflict, nothing is done.
+ *
+ * @param newFlarmId the new Flarm ID value (may or may not be different from
+ *                   the old value)
+ * @param oldFlarmId the old Flarm ID value (as it currently is in the database)
  * @return true if OK, false if canceled
  */
 bool FlarmIdCheck::interactiveCheck (const QString &newFlarmId, dbId planeId, const QString &oldFlarmId)
@@ -156,16 +269,11 @@ bool FlarmIdCheck::interactiveCheck (const QString &newFlarmId, dbId planeId, co
 
 	conflictingPlane.clearValue ();
 
-	// Nothing to do if the new Flarm ID is empty
-	if (newFlarmId.trimmed ().isEmpty ())
-		return true;
-
-	// Find the conflicting plane, i. e. the plane that alrady has the Flarm ID
-	// we're trying to set.
+	// Find a conflicting plane, i. e. a plane that alrady has the Flarm ID
+	// we're trying to set, unless the new Flarm ID is blank.
 	// FIXME there may be multiple
 	// FIXME: the old Flarm ID may also be a duplicate (but we're probably not
 	// going to handle that)
-
 	conflictingPlane=findConflictingPlane ();
 	if (!conflictingPlane.isValid ())
 		return true;
@@ -181,7 +289,22 @@ bool FlarmIdCheck::interactiveCheck (const QString &newFlarmId, dbId planeId, co
 		return true;
 }
 
-bool FlarmIdCheck::interactiveApply (QString *flightFlarmId)
+/**
+ * Applies the resolution selected by the user
+ *
+ * This method may only be called after calling interactiveCheck, but regardless
+ * of the return value of interactiveCheck.
+ *
+ * If the other plane has to be changed, the database is updated.
+ *
+ * if the plane's Flarm ID has to be changed, the value is written via the
+ * pointer passed as an argument (unless the pointer is NULL).
+ *
+ * @param flightFlarmId a pointer to the plane's Flarm ID. This should not be
+ *                      NULL as the value may have to be changed.
+ * @return false if the user canceled, or true otherwise
+ */
+bool FlarmIdCheck::interactiveApply (QString *flightFlarmId) // FIXME planeFlarmId
 {
 	// By default, use the new Flarm ID for the flight. We'll change that later
 	// if required by the user's choice.
--- src/flarm/FlarmIdCheck.h
+++ src/flarm/FlarmIdCheck.h
@@ -11,6 +11,34 @@
 class DbManager;
 class QWidget;
 
+/**
+ * A helper class to check for Flarm ID conflicts when updating a plane
+ *
+ * The Flarm ID of a plane should be unique, that is, there should be only one
+ * plane with a given Flarm ID at a time. However, Flarm IDs may change as Flarm
+ * units are moved around between planes. When creating or updating a plane, we
+ * want to check for a conflict and offer the user some choices to resolve the
+ * conflict. The selection of options is non-trivial; additionally, this has to
+ * be done in several places. This class contains the required functionality.
+ *
+ * When updating a plane, create an instance of FlarmIdCheck. Call the
+ * interactiveCheck method, passing the new Flarm ID, the old Flarm ID and the
+ * plane ID. This will determine if there is a conflict and, if so, ask the user
+ * for a resolution.
+ *
+ * You can then proceed to perform other checks, if required. When finished,
+ * call the interactiveApply method of the FlarmIdCheck instance. This will
+ * perform all the required changes to the database (allowing the user to
+ * cancel, therefore "interactive"). The plane's Flarm ID may have to be changed
+ * as well (e. g. if the user chooses to swap the Flarm ID with the conflicting
+ * plane), so a pointer to it must be passed to interactiveApply.
+ *
+ * Both interactiveCheck and interactiveApply will return false if the user
+ * cancels and true otherwise. If the user canceled during interactiveCheck,
+ * interactiveApply will do nothing, so you may choose to ignore the return
+ * value of interactiveCheck if you are going to call interactiveApply right
+ * away.
+ */
 class FlarmIdCheck: public QObject
 {
 		Q_OBJECT
@@ -25,34 +53,34 @@ class FlarmIdCheck: public QObject
 		bool keepOldFlarmId ();
 
 	protected:
-		// The reaction to a conflict
-		enum Reaction
+		/** The resolution of a conflict */
+		enum Resolution
 		{
 			clear,  // Set the other plane's Flarm ID to ""
 			swap,   // Set the other plane's Flarm ID this plane's old Flarm ID
-			ignore, // Ignore the conflict, both planes will have the same Flarm ID
-			keep,   // Keep this plane's old Flarm ID (cancel the Flarm ID update)
+			keep,   // Keep this plane's old Flarm ID
+			ignore, // Ignore the conflict (both planes will have the same Flarm ID)
 			cancel  // Cancel the whole operation
 		};
 
-		// A textual representation of a (possible) reaction for displaying to
-		// the user.
-		static QString Reaction_getText (Reaction reaction, const Plane &conflictingPlane, const QString &newFlarmId, const QString &oldFlarmId);
-
-
-		// A set of reactions presented to a user in case of a conflict. The set
-		// of reactions as wells as the default (preselected) reaction depend on
-		// the type of conflict.
+		/**
+		 * A set of reactions presented to a user in case of a conflict. The
+		 * set of reactions as wells as the default (preselected) reaction
+		 * depend on the type of conflict.
+		 */
+		// FIXME name - ResolutionSet?
 		class Options
 		{
 			public:
-				QList<Reaction> reactions;
-				Reaction defaultReaction;
+				// FIXME resolution, not reaction - all in file
+				QList<Resolution> reactions;
+				Resolution defaultReaction;
 		};
 
 		Maybe<Plane> findConflictingPlane ();
 		Options getOptions ();
-		Reaction showChoiceDialog (const Options &options);
+		QString makeText (Resolution reaction);
+		Resolution showChoiceDialog (const Options &options);
 
 	private:
 		DbManager &dbManager;
@@ -65,7 +93,7 @@ class FlarmIdCheck: public QObject
 
 		// Status
 		Maybe<Plane> conflictingPlane; // The conflicting plane if there is a conflict
-		Reaction selectedReaction;     // The reaction chosen by the user
+		Resolution selectedReaction;   // The reaction chosen by the user
 
 };
 
