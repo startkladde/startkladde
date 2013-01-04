8c693e7ff1a497e328dbce017295f1c8ba3101e0
Martin Herrmann <martin@famherrmann.de>
2013-01-04 14:46:14 +0000

    Bugfix: all values removed, but only one of them deleted
    git-svn-id: svn://svn.code.sf.net/p/startkladde/code/branches/flarm@1847 ffb59c96-f4eb-4fd4-83d4-e68da7e46744

--- src/util/qHash.h
+++ src/util/qHash.h
@@ -18,17 +18,18 @@ template<class K, class V> void clearAndDelete (QHash<K, V *> &hash)
  * Removes a value from the hash whose values are pointers and deletes it
  *
  * If the item does not exist in the hash, nothing happens. If there are
- * multiple items for the key in the hash, FIXME
- *
+ * multiple items for the key in the hash, all of them will be deleted and
+ * removed.
  */
 template<class K, class V> void removeAndDeleteIfExists (QHash<K, V *> &hash, const K &key)
 {
-	V *value=hash.value (key);
-	if (value)
-	{
-		hash.remove (key);
+	// Delete all values for that key
+	QList<V *> values=hash.values (key);
+	foreach (V *value, values)
 		delete value;
-	}
+
+	// Remove all values for that key
+	hash.remove (key);
 }
 
 #endif
