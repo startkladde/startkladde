0e6bcc6d65e89f5d09575a6fc50a871a1cac8f35
Martin Herrmann <martin@famherrmann.de>
2013-01-18 22:30:58 +0000

    Add script for sending a file to a serial port
    git-svn-id: svn://svn.code.sf.net/p/startkladde/code/branches/flarm@1886 ffb59c96-f4eb-4fd4-83d4-e68da7e46744

---
+++ script/client/serial_send.rb
@@ -0,0 +1,29 @@
+#!/usr/bin/env ruby
+
+def usage
+	puts "Sends one file/com port/... to another, one line at a time, with a configurable delay between lines"
+	puts "The delay is specified in seconds."
+	puts "Usage: #{$0} output delay input"
+	puts "Example: #{$0} COM8: 0.1 file.txt"
+	exit 1
+end
+
+usage if ARGV.size<3
+
+output=ARGV[0]
+delay=ARGV[1].to_f
+input=ARGV[2]
+
+begin
+	File.open(output, "w") { |p|
+		p.sync=true
+		File.open(input, "r").each { |line|
+			puts "Sending: "+line
+			p.puts line
+			sleep delay
+		}
+	}
+rescue Exception => ex
+	p ex
+end
+
